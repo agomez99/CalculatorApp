619b92387fe787f33711c2293051c37bbb9d6b1f
agomez99 <austine.gomez@yahoo.com>
2023-07-18 13:17:50 -0500

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log('Hello!');
+function appendValue(value) {
+  document.getElementById("result").value += value;
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function deleteLastCharacter() {
+  var result = document.getElementById("result").value;
+  document.getElementById("result").value = result.slice(0, -1);
+}
+
+function calculate() {
+  var result = eval(document.getElementById("result").value);
+  document.getElementById("result").value = result;
+}
+
+
+var buttons = document.getElementsByClassName("button");
+var arr = [...buttons];
+
+arr.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    element.style.opacity = "1";
+    if (index == 0) {
+      document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+    } else if (index == 1) {
+      document.getElementsByTagName("body")[0].style.backgroundColor = "teal";
+    } else {
+      document.getElementsByTagName("body")[0].style.backgroundColor =
+        "rgb(92, 204, 125)";
+    }
+    arr
+      .filter(function (item) {
+        return item != element;
+      })
+      .forEach((item) => {
+        item.style.opacity = "0";
+      });
+  });
+});
+
